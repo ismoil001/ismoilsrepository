de393f8abc8f5bc4c96b6ec72ef4453e2e42dd0f
alishersgitgub <alisheratadjanov1987@gmail.com>
2019-10-01 18:51:27 +0500

    18:50

--- app-order-service/src/main/java/uz/pdp/apporderservice/payload/ReqPdf.java
+++ app-order-service/src/main/java/uz/pdp/apporderservice/payload/ReqPdf.java
@@ -4,10 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReqPdf {
+    private UUID orderId;
     private String customerCompanyName;
     private String customerCompanyDirector;
+
+    public ReqPdf(String customerCompanyName, String customerCompanyDirector) {
+        this.customerCompanyName = customerCompanyName;
+        this.customerCompanyDirector = customerCompanyDirector;
+    }
 }
+
